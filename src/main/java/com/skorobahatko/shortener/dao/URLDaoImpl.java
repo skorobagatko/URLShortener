0248ec6d627bb34a237068239a20f26c24d07d68
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-10-05 23:23:33 +0300

    Fix session obtaining

--- src/main/java/com/skorobahatko/shortener/dao/URLDaoImpl.java
+++ src/main/java/com/skorobahatko/shortener/dao/URLDaoImpl.java
@@ -3,15 +3,14 @@ package com.skorobahatko.shortener.dao;
 import com.skorobahatko.shortener.exception.DatabaseURLGetException;
 import com.skorobahatko.shortener.exception.DatabaseURLSaveException;
 import com.skorobahatko.shortener.model.URL;
-import com.skorobahatko.shortener.exception.DatabaseException;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class URLDaoImpl implements URLDao {
@@ -23,29 +22,33 @@ public class URLDaoImpl implements URLDao {
     }
 
     @Override
+    @Transactional
     public long save(final URL url) throws DatabaseURLSaveException {
         try {
             Session session = sessionFactory.getCurrentSession();
             session.persist(url);
-            return getId(url);
+            return getId(session, url);
         } catch (HibernateException e) {
             throw new DatabaseURLSaveException("Can't save URL to database", e);
         }
     }
 
     @Override
+    @Transactional
     public URL getById(long id) throws DatabaseURLGetException {
         try {
-            Session session = sessionFactory.getCurrentSession();
+            Session session = sessionFactory.openSession();
             return session.load(URL.class, id);
         } catch (HibernateException e) {
-            throw new DatabaseURLGetException("Can't save URL to database", e);
+            throw new DatabaseURLGetException("Can't get URL from database", e);
         }
     }
 
-    private long getId(final URL url) throws HibernateException {
-        Session session = sessionFactory.getCurrentSession();
-        return session.load(URL.class, url.getUrl()).getId();
+    private long getId(Session session, final URL originalURL) throws HibernateException {
+        Query query = session.createQuery("FROM URL WHERE value = :value");
+        query.setParameter("value", originalURL.getUrl());
+        List<URL> list = query.list();
+        return list.get(0).getId();
     }
 
 }
