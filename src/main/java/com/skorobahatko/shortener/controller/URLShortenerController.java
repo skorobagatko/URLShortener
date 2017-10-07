6981f593e635f4a55fd28173e6cdefc9e8aeecc1
Stanislav Skorobahatko <skorobagatko@mail.com>
2017-10-07 22:06:04 +0300

    Add logic for saving and getting urls

--- src/main/java/com/skorobahatko/shortener/controller/URLShortenerController.java
+++ src/main/java/com/skorobahatko/shortener/controller/URLShortenerController.java
@@ -1,9 +1,15 @@
 package com.skorobahatko.shortener.controller;
 
+import com.skorobahatko.shortener.exception.DatabaseException;
 import com.skorobahatko.shortener.service.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class URLShortenerController {
@@ -14,5 +20,30 @@ public class URLShortenerController {
     @Qualifier(value = "urlService")
     public void setUrlService(URLService urlService) {
         this.urlService = urlService;
+    }
+
+    @RequestMapping(value = "/short", method = RequestMethod.POST)
+    public String getShortenedUrl(@RequestParam("url") String url) {
+        try {
+            String shortURL = urlService.getShortenedURL(url);
+            System.out.println(shortURL);
+            return "index";
+        } catch (DatabaseException e) {
+            //TODO: redirect to main page and print some error message
+            e.printStackTrace();
+            return "index";
+        }
+    }
+
+    @RequestMapping(value = "/{shortUrl}", method = RequestMethod.GET)
+    public String getOriginalUrl(@PathVariable String shortUrl) {
+        try {
+            String originalURL = urlService.getOriginalURL(shortUrl);
+            return "redirect:" + originalURL;
+        } catch (DatabaseException e) {
+            //TODO: redirect to main page and print some error message
+            e.printStackTrace();
+            return "index";
+        }
     }
 }
